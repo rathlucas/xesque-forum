0ad734c939a0a72d65d8dd657621b00651dfb999
Lucas Santiago Santana <lucin189@gmail.com>
2024-03-05 19:57:42 -0300

    funfa ae

--- xesque-forum-backend/src/main/java/dev/lucin/xesqueforum/domain/entity/UserEntity.java
+++ xesque-forum-backend/src/main/java/dev/lucin/xesqueforum/domain/entity/UserEntity.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.Collections;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
@@ -16,6 +17,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 @Setter
 @Document
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserEntity implements UserDetails {
 
     @Id
