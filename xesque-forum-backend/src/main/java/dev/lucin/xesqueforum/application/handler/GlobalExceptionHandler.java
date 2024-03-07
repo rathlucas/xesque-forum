4f82b85cf6157ef81be9bbe424b47f7d94f63e61
Lucas Santiago Santana <lucin189@gmail.com>
2024-03-06 22:05:34 -0300

    SCRUM-3 feat: add logback structured logging

--- xesque-forum-backend/src/main/java/dev/lucin/xesqueforum/application/handler/GlobalExceptionHandler.java
+++ xesque-forum-backend/src/main/java/dev/lucin/xesqueforum/application/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package dev.lucin.xesqueforum.application.handler;
 
 import dev.lucin.xesqueforum.application.exception.dto.ApiExceptionDTO;
 import java.util.ArrayList;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -10,11 +11,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.support.WebExchangeBindException;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(WebExchangeBindException.class)
     public ResponseEntity<Mono<ApiExceptionDTO>> handleWebExchangeBindException(WebExchangeBindException e) {
+        log.error("WebExceptionException: {}", e.getLocalizedMessage());
         var error = ApiExceptionDTO.builder()
                 .message(e.getLocalizedMessage())
                 .error(e.getStatusCode().toString().toUpperCase())
@@ -26,6 +29,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<Mono<ApiExceptionDTO>> handleBadCredentialsException(BadCredentialsException e) {
+        log.error("AuthenticationException: {}", e.getLocalizedMessage());
         var error = ApiExceptionDTO.builder()
                 .message(e.getLocalizedMessage())
                 .error(HttpStatus.UNAUTHORIZED.getReasonPhrase())
