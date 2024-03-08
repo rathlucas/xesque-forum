36baa61950e448da6c5dc526a238738f59358d99
Lucas Santiago Santana <lucin189@gmail.com>
2024-03-08 11:14:36 -0300

    SCRUM-35 feat: add jwt token management

--- xesque-forum-frontend/src/api/AxiosConfig.tsx
+++ xesque-forum-frontend/src/api/AxiosConfig.tsx
@@ -1,17 +1,20 @@
 import axios from "axios";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
-    Authorization: `Bearer `,
+    Authorization: `Bearer ${cookies().get("token")?.value}`,
   },
   timeout: 7000,
 });
 
 api.interceptors.response.use((res) => {
   if (res.status === 401) {
-    window.location.replace("/");
+    redirect("/");
+    cookies().delete("token");
     return res;
   }
   return res;
