5d88ab33d2ad5b0154486aed2ee2ebdfb383e706
Lucas Santiago Santana <lucin189@gmail.com>
2024-03-08 11:24:01 -0300

    SCRUM-35 test: update test suites

--- xesque-forum-backend/src/test/java/dev/lucin/xesqueforum/presentation/controller/AuthControllerTest.java
+++ xesque-forum-backend/src/test/java/dev/lucin/xesqueforum/presentation/controller/AuthControllerTest.java
@@ -1,5 +1,6 @@
 package dev.lucin.xesqueforum.presentation.controller;
 
+import dev.lucin.xesqueforum.domain.model.request.LoginRequest;
 import dev.lucin.xesqueforum.domain.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class AuthControllerTest {
@@ -28,6 +33,9 @@ class AuthControllerTest {
 
     @Test
     void givenValidCredentials_whenLoggingIn_shouldSuccessfullyBeAuthenticated() {
+
+        when(userService.login(any(LoginRequest.class)))
+                .thenReturn(Mono.just("token"));
 
         webTestClient.post()
                 .uri("/auth/login")
