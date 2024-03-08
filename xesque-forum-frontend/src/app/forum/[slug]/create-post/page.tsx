017d91a189d5f1830f88be40aa0cd8553cb1a65c
Lucas Santiago Santana <lucin189@gmail.com>
2024-03-08 15:37:10 -0300

    SCRUM-4 feat: create create-post-page and submission form

--- xesque-forum-frontend/src/app/forum/[slug]/create-post/page.tsx
+++ xesque-forum-frontend/src/app/forum/[slug]/create-post/page.tsx
@@ -1,10 +1,12 @@
 import NavigationBar from "@/components/navigation/NavigationBar";
+import CreatePostForm from "@/components/form/CreatePostForm";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
       <NavigationBar />
-      <h1>Criar postagem</h1>
+      <h1 className="text-xl my-3">Criação de postagem</h1>
+      <CreatePostForm />
     </main>
   );
 }
