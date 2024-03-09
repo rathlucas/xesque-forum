2eb808a5fef5ef71f8ae21e23333e48a8472713c
Lucas Santiago Santana <lucin189@gmail.com>
2024-03-09 13:55:02 -0300

    SCRUM update error.tsx component

--- xesque-forum-frontend/src/app/error.tsx
+++ xesque-forum-frontend/src/app/error.tsx
@@ -5,10 +5,10 @@ import { useEffect } from "react";
 export default function Error({
   error,
   reset,
-}: {
+}: Readonly<{
   error: Error & { digest?: string };
   reset: () => void;
-}) {
+}>) {
   useEffect(() => {
     console.error(error);
   }, [error]);
